b52474a3ab1c1220234c2634b09f4ec7bb17bbc6
Daniele Santos <js_dani@hotmail.com>
2020-11-14 19:30:00 -0300

    Configure App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './Home';
+import Home from './components/Home';
+import Details from './components/Details';
+
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/details" />
+        <Route path="/:id" element={<Details />} />
       </Routes>
+
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
